36bfb67522f009ce400444e86a89c0e83a8bd268
Zak Laughton <zlaughton@gmail.com>
2018-12-26 15:02:38 -0600

    Add profile endpoint logic

--- server.js
+++ server.js
@@ -70,17 +70,16 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user)
-    } 
+  db.select('*').from('users').where({id})
+    .then(user => {
+      console.log(user)
+      if (user.length) {
+        res.json(user[0])        
+      } else {
+        res.status(400).json('Not found')
+      }
   })
-
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
