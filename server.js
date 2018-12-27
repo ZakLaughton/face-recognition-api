ada5a587c4de7ab779f1f09b8a75d87679060980
Zak Laughton <zlaughton@gmail.com>
2018-12-27 16:29:33 -0600

    Move private API key to backend

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db)})
 
 app.listen(3000, () => {
   console.log('app is runing on port 3000');
